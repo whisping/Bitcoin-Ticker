61d30e9283a5e0b23663bbc36b6e70e649e55bb0
whisping <whisping@gmail.com>
2019-08-06 03:45:30 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    console.log(req.body.crypto);
+    console.log(req.body.fiat);
+});
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
+});
